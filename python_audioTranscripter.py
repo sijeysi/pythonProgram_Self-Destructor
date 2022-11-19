fcb0b4730d935526fb9b6b1fe6b3be211caab867
Crystal Jane Cadimas <crystaljanecadimas@iskolarngbayan.pup.edu.ph>
2022-11-19 20:54:27 +0800

    Added comments

--- python_audioTranscripter.py
+++ python_audioTranscripter.py
@@ -1,3 +1,13 @@
+# codes from Hallden "https://www.youtube.com/watch?v=_5pH_tr7uN0"
+# I'm interested in this program —which allows the device owner to delete his/her files when someone robbed 
+# him/her, or similar case, since I always lost my phone and I'm afraid when my info will leak.
+
+# Idea: As I've said above, we can use this program to delete all files on a lost device to avoid invasion of privacy.
+# We can improve this to only pop-up when the owner activate it using other trusted device. This can be a good
+# type of security.
+
+
+
 from tkinter import *
 import os
 
