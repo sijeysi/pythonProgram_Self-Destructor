d06a5066ca76a50c871e1c85d1025acbea21318a
Crystal Jane Cadimas <crystaljanecadimas@iskolarngbayan.pup.edu.ph>
2022-11-19 20:44:44 +0800

    Testing the program

--- python_audioTranscripter.py
+++ python_audioTranscripter.py
@@ -6,8 +6,8 @@ e = Entry(master)
 e.pack()
 e.focus_set()
 
-def callvbak():
-    if e.get() == password:
+def callback():
+    if e.get() == "password":
         quit()
 
 b = Button(master, text = "STOP", width=10, command=callback)
